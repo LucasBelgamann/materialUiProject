74663d564f3cc4978f812587fef37791e93a996a
Lucas Agostinho <lucasbelgamann@hotmail.com>
2023-10-01 23:12:15 -0300

    First commit

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { AppBar, Toolbar } from "@mui/material";
+
+export default function Header() {
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                Arc Development
+            </Toolbar>
+        </AppBar>
+    )
+}
