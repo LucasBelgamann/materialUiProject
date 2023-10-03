293dd5ba6168aa0cda05542521b2adb195edb695
Lucas Agostinho <lucasbelgamann@hotmail.com>
2023-10-02 23:04:04 -0300

    Feat: add colors to theme

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,31 +1,34 @@
 import { AppBar, Toolbar } from "@mui/material";
-import useScrollTrigger from '@mui/material/useScrollTrigger';
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 import React from "react";
+import Typography from "@mui/material/Typography";
 
 interface Props {
-    children: React.ReactElement;
-  }
+  children: React.ReactElement;
+}
 
 function ElevationScroll(props: Props) {
-    const { children } = props;
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
 
-export default function Header() {
-    return (
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default function Header(props) {
+  return (
+    <>
       <ElevationScroll>
         <AppBar position="fixed">
-            <Toolbar>
-                Arc Development
-            </Toolbar>
+          <Toolbar>
+            <Typography variant="h3">Arc Development</Typography>
+          </Toolbar>
         </AppBar>
       </ElevationScroll>
-    )
+    </>
+  );
 }
--- src/components/ui/Theme.ts
+++ src/components/ui/Theme.ts
@@ -1,5 +1,31 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
+
+const arcBlue = "#0B7289";
+const arcOrange = "#FFBA60";
+
+declare module "@mui/material/styles" {
+  interface CommonColors {
+    blue: string;
+    orange: string;
+  }
+}
 
 export default createTheme({
-    
-})
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+  typography: {
+    h3: {
+        fontWeight: 300,
+    }
+  }
+});
