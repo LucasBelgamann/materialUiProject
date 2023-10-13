475a3282c2c11d25ad9bb4378fc3ab39e3c83a9f
Lucas Agostinho <lucasbelgamann@hotmail.com>
2023-10-12 22:38:52 -0300

    Feat: Novos estilos

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,8 +1,8 @@
-import { createTheme } from "@mui/material";
-
+import { createTheme } from "@mui/material/styles";
 
 const arcBlue = "#0B72B9";
 const arcOrange = "#FFBA60";
+const arcGrey = "#868686";
 
 export default createTheme({
   palette: {
@@ -31,5 +31,84 @@ export default createTheme({
       textTransform: "none",
       color: "white",
     },
+    h2: {
+      fontFamily: "Raleway",
+      fontWeight: 700,
+      fontSize: "2.5rem",
+      color: arcBlue,
+      lineHeight: 1.5,
+    },
+    h3: {
+      fontFamily: "Pacifico",
+      fontSize: "2.5rem",
+      color: arcBlue,
+    },
+    h4: {
+      fontFamily: "Raleway",
+      fontSize: "1.75rem",
+      color: arcBlue,
+      fontWeight: 700,
+    },
+    h6: {
+      fontWeight: 500,
+      fontFamily: "Raleway",
+      color: arcBlue,
+    },
+    subtitle1: {
+      fontSize: "1.25rem",
+      fontWeight: 300,
+      color: arcGrey,
+    },
+    subtitle2: {
+      color: "white",
+      fontWeight: 300,
+      fontSize: "1.25rem",
+    },
+    body1: {
+      fontSize: "1.25rem",
+      color: arcGrey,
+      fontWeight: 300,
+    },
+    caption: {
+      fontSize: "1rem",
+      fontWeight: 300,
+      color: arcGrey,
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      borderWidth: 2,
+      textTransform: "none",
+      color: arcBlue,
+      borderRadius: 50,
+      fontFamily: "Roboto",
+      fontWeight: "bold",
+    },
+  },
+  components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: arcBlue,
+          fontSize: "1rem",
+        },
+      },
+    },
+    MuiInput: {
+      styleOverrides: {
+        root: {
+          color: arcGrey,
+          fontWeight: 300,
+        },
+        underline: {
+          "&:before": {
+            borderBottom: `2px solid ${arcBlue}`,
+          },
+          "&:hover:not($disabled):before": {
+            borderBottom: `2px solid ${arcBlue}`,
+          },
+        },
+      },
+    },
   },
 });
+
